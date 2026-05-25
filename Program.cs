5c59e9c8d1292243686433df2204eef86ac032ef
Facundo <diazortizfacundo@gmail.com>
2026-05-25 14:39:38 -0300

    ejercicio4

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ Console.WriteLine("valor de b:"+b);
 */
 
 //EJERCICIO 1
-/*Console.WriteLine("----EJERCICIO 1----");
+Console.WriteLine("----EJERCICIO 1----");
 
 Console.Write("Ingrese un Numero para invertirlo: ");
 string numeroIngresado = Console.ReadLine();
@@ -34,11 +34,13 @@ if(int.TryParse(numeroIngresado, out i))
 else
 {
     Console.WriteLine("La linea ingresada no es un numero");
-}*/
+}
 
 //EJERCICIO 2
+
 int opcion, num1, num2;
 int reintentar = 1;
+
 
 while(reintentar == 1)
 {
@@ -132,3 +134,129 @@ Console.WriteLine("---------------------------");
 Console.WriteLine($"Maximo entre los dos numeros ingresados {Math.Max(num3,num4)}");
 Console.WriteLine($"Minimo entre los dos numeros ingresados {Math.Min(num3,num4)}");
 Console.WriteLine("---------------------------");
+
+//EJERCICIO 4
+Console.Write("Ingrese una cadena: ");
+string s = Console.ReadLine();
+
+Console.WriteLine($"Longitud de la cadena: {s.Length}");
+
+Console.Write("Ingrese una segunda cadena para concatenar con la primera: ");
+string s1 = Console.ReadLine();
+
+Console.WriteLine($"Cadena 1 concatenada con la Cadena 2: {s}{s1}");
+
+Console.WriteLine($"Subcadena de Cadena 1: {s.Substring(0,2)}");
+
+int numero1, numero2, resultado;
+Console.Write("Ingrese un numero1: ");
+while(!int.TryParse(Console.ReadLine(), out numero1))
+{
+    Console.Write("Lo ingresado no es un numero, intente denuevo: ");
+}
+Console.Write("Ingrese un numero2: ");
+while(!int.TryParse(Console.ReadLine(), out numero2))
+{
+    Console.Write("Lo ingresado no es un numero, intente denuevo: ");
+}
+
+    Console.WriteLine("----Calculadora V1----");
+    Console.WriteLine("Opcion 1: SUMAR");
+    Console.WriteLine("Opcion 2: RESTAR");
+    Console.WriteLine("Opcion 3: MULTIPLICAR");
+    Console.WriteLine("Opcion 4: DIVIDIR");
+    Console.Write("Elija una opcion: ");
+    int.TryParse(Console.ReadLine(), out opcion);
+
+    while(opcion != 1 && opcion != 2 && opcion != 3 && opcion != 4)
+    {
+        Console.WriteLine("------------------------------");
+        Console.Write("error, ingrese la opcion denuevo: ");
+        int.TryParse(Console.ReadLine(), out opcion);
+    }
+
+    switch (opcion)
+    {
+        case 1:
+        resultado = numero1 + numero2;
+        Console.WriteLine("La suma entre "+ numero1.ToString() +" y "+ numero2.ToString() +" es igual a: "+ resultado.ToString() );
+        break;
+
+        case 2:
+        resultado = numero1 - numero2;
+        Console.WriteLine("La resta entre "+ numero1.ToString() +" y "+ numero2.ToString() +" es igual a: "+ resultado.ToString() );
+        break;
+
+        case 3:
+        resultado = numero1 * numero2;
+        Console.WriteLine("La Multiplicacion entre "+ numero1.ToString() +" y "+ numero2.ToString() +" es igual a: "+ resultado.ToString() );
+        break;
+
+        case 4:
+        resultado = numero1 / numero2;
+        Console.WriteLine("La division entre "+ numero1.ToString() +" y "+ numero2.ToString() +" es igual a: "+ resultado.ToString() );
+        break;
+    }
+
+Console.WriteLine("Cadena 1 recorrida con for each: ");
+foreach(char caracter in s)
+{
+    Console.WriteLine(caracter);
+}
+
+
+Console.WriteLine("Ingrese una subcadena para buscar su ocurrecia en Cadena 1: ");
+string s2 = Console.ReadLine();
+
+if(s.IndexOf(s2) != -1)
+{
+    Console.WriteLine("La subcadena "+ s2 + ", SI se encuentra en la Cadena 1");
+}
+else
+{
+    Console.WriteLine("La subcadena "+ s2 + ", NO se encuentra en la Cadena 1");
+}
+
+Console.WriteLine($"La cadena 1 en mayusculas es: {s.ToUpper()}");
+Console.WriteLine($"La cadena 1 en minusculas es: {s.ToLower()}");
+
+
+Console.WriteLine("Ingrese una cadena separada que este separada por un caracter especifico: ");
+string s3 = Console.ReadLine();
+
+Console.WriteLine("Ingrese el caracter: ");
+char caracterEspeficico = Console.ReadLine()[0];
+
+foreach(string palabra in s3.Split(caracterEspeficico))
+{
+    Console.WriteLine(palabra);
+}
+
+
+Console.WriteLine("Ingrese una ecuacion simple: ");
+string ecuacion = Console.ReadLine();
+int resultadoEcuacion = 0;
+
+if(ecuacion.IndexOf("+") != -1)
+{
+    string[] terminos = ecuacion.Split('+');
+    resultadoEcuacion = int.Parse(terminos[0]) + int.Parse(terminos[1]);
+}
+if(ecuacion.IndexOf("-") != -1)
+{
+    string[] terminos = ecuacion.Split('-');
+    resultadoEcuacion = int.Parse(terminos[0]) - int.Parse(terminos[1]);
+}
+if(ecuacion.IndexOf("*") != -1)
+{
+    string[] terminos = ecuacion.Split('*');
+    resultadoEcuacion = int.Parse(terminos[0]) * int.Parse(terminos[1]);
+}
+if(ecuacion.IndexOf("/") != -1)
+{
+    string[] terminos = ecuacion.Split('/');
+    resultadoEcuacion = int.Parse(terminos[0]) / int.Parse(terminos[1]);
+}
+
+Console.WriteLine("Resultado de la ecuacion: "+ resultadoEcuacion);
+
---
+++ README.md
@@ -0,0 +1,21 @@
+#Ejercicio 4
+##¿String es una tipo por valor o un tipo por referencia?
+String es un tipo por referencia
+##¿Qué secuencias de escape tiene el tipo string?
+\'	Comilla simple
+\"	Comilla doble	
+\\	Barra invertida
+\0	Nulo	
+\a	Alerta
+\b	Retroceso
+\e	Escapar
+\f	Avance de página
+\n	Nueva línea
+\r	Retorno de carro
+\t	Tabulación horizontal
+\v	Tabulación vertical
+\u	Secuencia de escape Unicode (UTF-16)	
+\U	Secuencia de escape Unicode (UTF-32)
+\x	Secuencia de escape Unicode similar a "\u" excepto con longitud variable
+##¿Qué sucede cuando utiliza el carácter @ y $ antes de una cadena de texto?
+cuando se utiliza "$" antes de una cadena de texto se declara una cadena interpolada, esta permite incluir variables dentro de llaves "{ }", por otro lado cuando se utiliza el caracter "@" se declara una cadena literal, este tipo de cadena se puede escribir en multiples lineas y las barras "\" no se interpretan como secuencia de escape
